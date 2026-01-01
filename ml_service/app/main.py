fb14fddb2a31e72584a4d3dee9bd945f7f59c92a
Arslan Rejebov <arslanrejepov174@gmail.com>
2026-01-02 00:22:28 +0800

    deploying...

--- ml_service/app/main.py
+++ ml_service/app/main.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+
+app = FastAPI()
+
+class PredictionRequest(BaseModel):
+    user_id : int
+    recent_products : List[int]
+
+class PredictionResponse(BaseModel):
+    recommended_products : List[int]
+
+@app.post("/predict",response_model=PredictionResponse)
+def predict(request : PredictionRequest):
+    user_id = request.user_id
+    recent_products = request.recent_products
+
+    recommended_products = get_recomendations(user_id,recent_products)
+    return PredictionResponse(recommended_products=recommended_products)
